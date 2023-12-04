66d7e9c05e27f52bf544503ae680dedeea934bbe
lwileczek <pleazenospam@protonmail.com>
2023-12-04 09:35:04 -0500

    finish day4

---
+++ day4/README.md
@@ -0,0 +1,11 @@
+# Day 4
+## Part 1
+I was pretty happy with how I didn't do any conversion from string to number nor call the split function. 
+Because of that, I thought it would be pretty efficient having to do fewer allocation.
+Made some assumptions about the format of the file which would make it difficult to use this elsewhere
+
+
+## Part 2
+I thought the question made it pretty obvious this is a recurson problem.
+When I ran my solution I was right on the first try but it was so terribly slow I know I wrote some bad code.
+I'll have to see how others did it to make the part 2 fast.
--- day4/src/main.rs
+++ day4/src/main.rs
@@ -7,6 +7,9 @@ fn main() -> Result<(), std::io::Error>{
     let result = data.iter().fold(0, |acc, game| acc + score_game(&game));
     print!("Part 1: {}\n", result);
 
+    let ans2 = count_tickets(&data);
+    print!("Part 2: {}\n", ans2);
+
     Ok(())
 }
 
@@ -21,6 +24,57 @@ fn read_file(file_name: &str) -> Result<Vec<String>, std::io::Error> {
     }
 
     Ok(v)
+}
+
+fn count_tickets(data: &Vec<String>) -> u32 {
+    let mut count: u32 = data.len() as u32;
+    for i in 0..data.len() {
+        count = count + score_game_part2(data, i);
+    }
+    count
+}
+
+fn score_game_part2(dataset: &Vec<String>, idx: usize) -> u32 {
+    let input = &dataset[idx];
+    let mut score = 0;
+    let mut pos = 0;
+    let mut winning_numbers: Vec<&str> = Vec::new();
+    for (i, c) in input.chars().enumerate() {
+        if c == ':' {
+            //ASSUMPTION: always a space after the semi-colon,
+            pos = i + 2;
+            break;
+        }
+    }
+    let charz: Vec<char> = input.chars().collect();
+    while pos < input.len() {
+        if charz[pos] == '|' {
+            pos = pos + 2;
+            break;
+        }
+        let n: &str = &input[pos..(pos+2)];
+        winning_numbers.push(n);
+        //ASSUMPTION: no winning number is above 99
+        pos = pos + 3;
+    }
+
+    while pos < input.len() {
+        let n: &str = &input[pos..(pos+2)];
+        for val in winning_numbers.iter() {
+            if **val == *n {
+                score = score + 1;
+                break;
+            }
+        }
+        
+        pos = pos + 3;
+    }
+    if score > 0 {
+        for k in 1..=score as usize {
+            score = score + score_game_part2(dataset, idx + k);
+        }
+    }
+    score
 }
 
 fn score_game(input: &String) -> u32 {
@@ -92,6 +146,19 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#.lines().collect();
         let s = convert_vec_str_to_vec_string(input);
         let score = score_game(&s[0]);
         assert_eq!(score, 8);
+    }
+
+    #[test]
+    fn test_counting_tickets() {
+        let input = r#"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#.lines().collect();
+        let data = convert_vec_str_to_vec_string(input);
+        let ans = count_tickets(&data);
+        assert_eq!(ans, 30);
     }
 
     fn convert_vec_str_to_vec_string(vec_str: Vec<&str>) -> Vec<String> {
