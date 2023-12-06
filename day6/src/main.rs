c67f08a6d9c0f09144a3378b719cfacdbfe5e67c
lwileczek <pleazenospam@protonmail.com>
2023-12-06 08:28:27 -0500

    day6p1

---
+++ day6/src/main.rs
@@ -0,0 +1,155 @@
+use std::str;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() -> Result<(), std::io::Error>{
+    let data = read_file("data.txt")?;
+
+    let ans1 = p1(&data);
+    println!("Part 1: {}", ans1);
+
+    //let ans2 = do_work(&data);
+    //println!("Part 2: {}", ans2);
+
+    Ok(())
+}
+
+//There is a trick where you only have to do halve the calculations because
+//of the symetric and parabolic nature of this problem, will try after first solution
+fn p1 (data: &Vec<String>) -> u32 {
+    let times = parse_nums(&data[0]);
+    let distances = parse_nums(&data[1]);
+    if times.len() != distances.len() {
+        println!("time: {} | dist: {}", times.len(), distances.len());
+        panic!("didn't match up times and distences correctly")
+    }
+    let mut ans: u32 = 1;
+    for (t, d) in times.iter().zip(distances.iter()) {
+        let wins = count_wins(t, d);
+        ans = ans * wins as u32;
+    }
+    ans
+}
+
+fn count_wins(t: &u16, d: &u16) -> u16 {
+    let mut tally = 0;
+    for k in 1..*t {
+        let run = project_distance(k, &t);
+        if run > *d {
+            tally = tally + 1;
+        }
+    }
+
+    tally
+}
+
+fn project_distance(t: u16, total: &u16) -> u16 {
+    if *total <= t {
+        return 0;
+    }
+    t * (total - t)
+}
+
+fn parse_nums(line: &String) -> Vec<u16>{
+    let charz: Vec<char> = line.chars().collect();
+    let mut ans: Vec<u16> = Vec::new();
+    let mut pos = 0;
+    while pos < line.len() {
+        if charz[pos].is_digit(10) {
+            let mut end = pos + 1;
+            while end < line.len() && line.chars().nth(end).unwrap().is_digit(10) {
+                end += 1;
+            }
+            let n = match line[pos..end].parse::<u16>() {
+                Ok(v) => v,
+                Err(e) => panic!("unable to parse u32 from string! {}", e)
+            };
+
+            ans.push(n);
+            pos = end;
+            continue
+        }
+        pos = pos + 1
+    }
+    ans
+}
+
+fn read_file(file_name: &str) -> Result<Vec<String>, std::io::Error> {
+    let file = File::open(file_name)?;
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+    let mut v: Vec<String> = Vec::new();
+
+    while let Some(Ok(line)) = lines.next() {
+        v.push(line);
+    }
+
+    Ok(v)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_getting_times() {
+        let input=r#"Time:      7  15   30
+Distance:  9  40  200"#.lines().collect();
+        let data = convert_vec_str_to_vec_string(input);
+        let n = p1(&data);
+        assert_eq!(n, 288);
+    }
+
+    #[test]
+    fn test_projecting_distance() {
+        let data = vec![
+            vec![0, 0],
+            vec![1, 6],
+            vec![2, 10],
+            vec![3, 12],
+            vec![4, 12],
+            vec![5, 10],
+            vec![6, 6],
+            vec![7, 0],
+        ];
+         let total: u16 = 7;
+        for input in data.iter() {
+            let n =  project_distance(input[0], &total);
+            assert_eq!(n, input[1]);
+        }
+    }
+
+    #[test]
+    fn test_counting_wins() {
+        let data = vec![
+            vec![7, 9, 4],
+            vec![15, 40, 8],
+            vec![30, 200, 9],
+        ];
+        for input in data.iter() {
+            let n =  count_wins(&input[0], &input[1]);
+            assert_eq!(n, input[2]);
+        }
+    }
+
+    #[test]
+    fn test_parsing_nums() {
+        let s = "Time:      7  15   30";
+        let nums = parse_nums(&s.to_string());
+        assert_eq!(nums.len(), 3);
+
+        assert_eq!(nums[0], 7);
+        assert_eq!(nums[1], 15);
+        assert_eq!(nums[2], 30);
+    }
+
+    fn convert_vec_str_to_vec_string(vec_str: Vec<&str>) -> Vec<String> {
+        let mut vec_string = Vec::new();
+        for s in vec_str {
+            match s {
+                s => vec_string.push(s.to_owned()),
+            }
+        }
+        vec_string
+    }
+}
